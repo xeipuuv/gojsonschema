db4b1280ee52cb1a9b00ab1489861808d542a27d
sigu-399 <sigu.399@gmail.com>
2013-02-26 22:43:21 +0800

    added consts to handle json schema keys

--- src/gojsonschema/schema.go
+++ src/gojsonschema/schema.go
@@ -17,6 +17,14 @@ const (
 	ENUM_VALUE_NOT_ALLOWED     = `%s '%s' is not allowed`
 )
 
+const (
+	KEY_SCHEMA      = `$schema`
+	KEY_ID          = `id`
+	KEY_TITLE       = `title`
+	KEY_DESCRIPTION = `description`
+	KEY_TYPE        = `type`
+)
+
 func NewJsonSchema(document interface{}) (JsonSchema, error) {
 
 	var s JsonSchema
@@ -46,60 +54,60 @@ func (s *JsonSchema) parse(document interface{}) error {
 	schemaMap := s.document.(map[string]interface{})
 
 	// $schema
-	if !mapValueExists(schemaMap, "$schema") {
-		return errors.New(fmt.Sprintf(IS_REQUIRED, "$schema"))
+	if !mapValueExists(schemaMap, KEY_SCHEMA) {
+		return errors.New(fmt.Sprintf(IS_REQUIRED, KEY_SCHEMA))
 	}
 
-	if !interfaceOfKind(schemaMap["$schema"], reflect.String) {
-		return errors.New(fmt.Sprintf(MUST_BE_STRING, "$schema"))
+	if !interfaceOfKind(schemaMap[KEY_SCHEMA], reflect.String) {
+		return errors.New(fmt.Sprintf(MUST_BE_STRING, KEY_SCHEMA))
 	}
 
-	s.schemaKeyword = schemaMap["$schema"].(string)
+	s.schemaKeyword = schemaMap[KEY_SCHEMA].(string)
 
 	if !isStringInList(SUPPORTED_SCHEMA_KEYWORDS, s.schemaKeyword) {
-		return errors.New(fmt.Sprintf(ENUM_VALUE_NOT_ALLOWED, "$schema", s.schemaKeyword))
+		return errors.New(fmt.Sprintf(ENUM_VALUE_NOT_ALLOWED, KEY_SCHEMA, s.schemaKeyword))
 	}
 
 	// type
-	if !mapValueExists(schemaMap, "type") {
-		return errors.New(fmt.Sprintf(IS_REQUIRED, "type"))
+	if !mapValueExists(schemaMap, KEY_TYPE) {
+		return errors.New(fmt.Sprintf(IS_REQUIRED, KEY_TYPE))
 	}
 
-	if !interfaceOfKind(schemaMap["type"], reflect.String) {
-		return errors.New(fmt.Sprintf(MUST_BE_STRING, "type"))
+	if !interfaceOfKind(schemaMap[KEY_TYPE], reflect.String) {
+		return errors.New(fmt.Sprintf(MUST_BE_STRING, KEY_TYPE))
 	}
 
-	s.typeKeyword = schemaMap["type"].(string)
+	s.typeKeyword = schemaMap[KEY_TYPE].(string)
 
 	if !isStringInList(SUPPORTED_TYPE_KEYWORDS, s.typeKeyword) {
-		return errors.New(fmt.Sprintf(ENUM_VALUE_NOT_ALLOWED, "type", s.typeKeyword))
+		return errors.New(fmt.Sprintf(ENUM_VALUE_NOT_ALLOWED, KEY_TYPE, s.typeKeyword))
 	}
 
 	// id
 
-	if mapValueExists(schemaMap, "id") {
-		if !interfaceOfKind(schemaMap["id"], reflect.String) {
-			return errors.New(fmt.Sprintf(MUST_BE_STRING, "id"))
+	if mapValueExists(schemaMap, KEY_ID) {
+		if !interfaceOfKind(schemaMap[KEY_ID], reflect.String) {
+			return errors.New(fmt.Sprintf(MUST_BE_STRING, KEY_ID))
 		}
-		s.idKeyword = schemaMap["id"].(string)
+		s.idKeyword = schemaMap[KEY_ID].(string)
 	}
 
 	// title
 
-	if mapValueExists(schemaMap, "title") {
-		if !interfaceOfKind(schemaMap["title"], reflect.String) {
-			return errors.New(fmt.Sprintf(MUST_BE_STRING, "title"))
+	if mapValueExists(schemaMap, KEY_TITLE) {
+		if !interfaceOfKind(schemaMap[KEY_TITLE], reflect.String) {
+			return errors.New(fmt.Sprintf(MUST_BE_STRING, KEY_TITLE))
 		}
-		s.titleKeyword = schemaMap["title"].(string)
+		s.titleKeyword = schemaMap[KEY_TITLE].(string)
 	}
 
 	// description
 
-	if mapValueExists(schemaMap, "description") {
-		if !interfaceOfKind(schemaMap["description"], reflect.String) {
-			return errors.New(fmt.Sprintf(MUST_BE_STRING, "description"))
+	if mapValueExists(schemaMap, KEY_DESCRIPTION) {
+		if !interfaceOfKind(schemaMap[KEY_DESCRIPTION], reflect.String) {
+			return errors.New(fmt.Sprintf(MUST_BE_STRING, KEY_DESCRIPTION))
 		}
-		s.descriptionKeyword = schemaMap["description"].(string)
+		s.descriptionKeyword = schemaMap[KEY_DESCRIPTION].(string)
 	}
 
 	return nil
