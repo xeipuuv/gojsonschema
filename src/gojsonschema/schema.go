af3908a85215078bd99a5bb3a8629d8b92de37fd
sigu-399 <sigu.399@gmail.com>
2013-02-27 00:15:01 +0800

    refactoring to handle references

--- src/gojsonschema/schema.go
+++ src/gojsonschema/schema.go
@@ -10,164 +10,44 @@ import (
 	"reflect"
 )
 
-const (
-	JSON_SCHEMA_MUST_BE_OBJECT = `Schema root must be an object`
-	MUST_BE_STRING             = `'%s' must be a string`
-	IS_REQUIRED                = `'%s' is required`
-	ENUM_VALUE_NOT_ALLOWED     = `%s '%s' is not allowed`
-)
-
-const (
-	KEY_SCHEMA      = `$schema`
-	KEY_ID          = `id`
-	KEY_TITLE       = `title`
-	KEY_DESCRIPTION = `description`
-	KEY_TYPE        = `type`
-)
-
-func NewJsonSchemaDocument(node interface{}) (JsonSchemaDocument, error) {
-
+func NewJsonSchemaDocument(document interface{}) (JsonSchemaDocument, error) {
 	var d JsonSchemaDocument
-	err := d.parse(node)
+	err := d.parse(document)
 	return d, err
-
 }
 
 type JsonSchemaDocument struct {
-	schemaKeyword string
-	rootSchema    JsonSchema
-	node          interface{}
 }
 
-type JsonSchema struct {
-	idKeyword          string
-	titleKeyword       string
-	descriptionKeyword string
-	typeKeyword        string
-	node               interface{}
+func (d *JsonSchemaDocument) parse(document interface{}) error {
+	return d.parseNodeRecursive(document)
 }
 
-func (d *JsonSchemaDocument) parse(node interface{}) error {
+func (d *JsonSchemaDocument) parseNodeRecursive(documentNode interface{}) error {
 
-	if !interfaceOfKind(node, reflect.Map) {
-		return errors.New(JSON_SCHEMA_MUST_BE_OBJECT)
-	}
+	rValue := reflect.ValueOf(documentNode)
+	rKind := rValue.Kind()
 
-	schemaMap := node.(map[string]interface{})
-	d.node = schemaMap
+	fmt.Printf("Kind : %s\n", rKind)
 
-	// $schema
-	if !mapValueExists(schemaMap, KEY_SCHEMA) {
-		return errors.New(fmt.Sprintf(IS_REQUIRED, KEY_SCHEMA))
-	}
+	switch rKind {
 
-	if !interfaceOfKind(schemaMap[KEY_SCHEMA], reflect.String) {
-		return errors.New(fmt.Sprintf(MUST_BE_STRING, KEY_SCHEMA))
-	}
-
-	d.schemaKeyword = schemaMap[KEY_SCHEMA].(string)
-
-	if !isStringInList(SUPPORTED_SCHEMA_KEYWORDS, d.schemaKeyword) {
-		return errors.New(fmt.Sprintf(ENUM_VALUE_NOT_ALLOWED, KEY_SCHEMA, d.schemaKeyword))
-	}
-
-	return d.rootSchema.parse(d.node)
-
-}
-
-func (s *JsonSchema) parse(node interface{}) error {
-
-	if !interfaceOfKind(node, reflect.Map) {
-		return errors.New(JSON_SCHEMA_MUST_BE_OBJECT)
-	}
-
-	schemaMap := node.(map[string]interface{})
-	s.node = schemaMap
-	
-	// type
-	if !mapValueExists(schemaMap, KEY_TYPE) {
-		return errors.New(fmt.Sprintf(IS_REQUIRED, KEY_TYPE))
-	}
-
-	if !interfaceOfKind(schemaMap[KEY_TYPE], reflect.String) {
-		return errors.New(fmt.Sprintf(MUST_BE_STRING, KEY_TYPE))
-	}
-
-	s.typeKeyword = schemaMap[KEY_TYPE].(string)
-
-	if !isStringInList(SUPPORTED_TYPE_KEYWORDS, s.typeKeyword) {
-		return errors.New(fmt.Sprintf(ENUM_VALUE_NOT_ALLOWED, KEY_TYPE, s.typeKeyword))
-	}
-
-	// id
-
-	if mapValueExists(schemaMap, KEY_ID) {
-		if !interfaceOfKind(schemaMap[KEY_ID], reflect.String) {
-			return errors.New(fmt.Sprintf(MUST_BE_STRING, KEY_ID))
+	case reflect.Map:
+		m := documentNode.(map[string]interface{})
+		for k := range m {
+			fmt.Printf("Key : %s\n", k)
+			err := d.parseNodeRecursive(m[k])
+			if err != nil {
+				return err
+			}
 		}
-		s.idKeyword = schemaMap[KEY_ID].(string)
-	}
 
-	// title
+	case reflect.String:
+	case reflect.Bool:
 
-	if mapValueExists(schemaMap, KEY_TITLE) {
-		if !interfaceOfKind(schemaMap[KEY_TITLE], reflect.String) {
-			return errors.New(fmt.Sprintf(MUST_BE_STRING, KEY_TITLE))
-		}
-		s.titleKeyword = schemaMap[KEY_TITLE].(string)
-	}
-
-	// description
-
-	if mapValueExists(schemaMap, KEY_DESCRIPTION) {
-		if !interfaceOfKind(schemaMap[KEY_DESCRIPTION], reflect.String) {
-			return errors.New(fmt.Sprintf(MUST_BE_STRING, KEY_DESCRIPTION))
-		}
-		s.descriptionKeyword = schemaMap[KEY_DESCRIPTION].(string)
+	default:
+		return errors.New(fmt.Sprintf("Unhandled kind : %s", rKind))
 	}
 
 	return nil
-}
-
-// Schema validation funcs
-
-var SUPPORTED_SCHEMA_KEYWORDS []string
-var SUPPORTED_TYPE_KEYWORDS []string
-
-func init() {
-	SUPPORTED_SCHEMA_KEYWORDS = []string{
-		`http://json-schema.org/schema#`,
-		//		`http://json-schema.org/hyper-schema#`,
-		`http://json-schema.org/draft-04/schema#`,
-		//		`http://json-schema.org/draft-04/hyper-schema#`,
-		//		`http://json-schema.org/draft-03/schema#`,
-		//		`http://json-schema.org/draft-03/hyper-schema#`,
-	}
-	SUPPORTED_TYPE_KEYWORDS = []string{
-		`array`,
-		`boolean`,
-		`integer`,
-		`number`,
-		`null`,
-		`object`,
-		`string`,
-	}
-}
-
-func interfaceOfKind(node interface{}, kind reflect.Kind) bool {
-	return reflect.ValueOf(node).Kind() == kind
-}
-
-func mapValueExists(m map[string]interface{}, key string) bool {
-	_, ok := m[key]
-	return ok
-}
-
-func isStringInList(list []string, keyword string) bool {
-	for _, v := range list {
-		if v == keyword {
-			return true
-		}
-	}
-	return false
 }
