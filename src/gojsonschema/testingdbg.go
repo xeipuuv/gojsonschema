802282ea329f34a17ee002c7b638070445abca48
sigu-399 <sigu.399@gmail.com>
2013-02-27 19:58:58 +0800

    added utility funcs for testing/debugging purposes

---
+++ src/gojsonschema/testingdbg.go
@@ -0,0 +1,23 @@
+package gojsonschema
+
+import (
+	"fmt"
+)
+
+func DebugDisplayJsonSchema(d *JsonSchemaDocument) {
+	debugDisplayJsonSchemaRecursive(d.rootSchema, 0)
+}
+
+func debugDisplayJsonSchemaRecursive(s *JsonSchema, level int) {
+	for i := 0; i != level; i++ {
+		fmt.Printf(" ")
+	}
+
+	if s.property == nil {
+		fmt.Printf("(nil)")
+	} else {
+		fmt.Printf(*s.property)
+	}
+
+	fmt.Printf("/n")
+}
