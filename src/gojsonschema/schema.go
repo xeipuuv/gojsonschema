0bda7b6f1adb5b592b57f0ba386c26427c4e45bc
sigu-399 <sigu.399@gmail.com>
2013-02-27 20:37:29 +0800

    added schema keyword

--- src/gojsonschema/schema.go
+++ src/gojsonschema/schema.go
@@ -4,18 +4,22 @@
 
 package gojsonschema
 
-import ()
+import (
+	"gojsonreference"
+)
 
 type JsonSchema struct {
 	id          *string
 	title       *string
-	description *string	
+	description *string
 
 	ref *string
 
 	children []*JsonSchema
 
 	property *string
+
+	schema *gojsonreference.JsonReference
 }
 
 func (s *JsonSchema) AddChild(child *JsonSchema) {
