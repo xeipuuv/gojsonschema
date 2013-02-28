744e11e43e6407f7e6fe8ee659137eead96f868b
sigu-399 <sigu.399@gmail.com>
2013-02-28 22:25:06 +0800

    added schemaType

---
+++ src/gojsonschema/schemaType.go
@@ -0,0 +1,40 @@
+// @author       sigu-399
+// @description  An implementation of JSON Schema, draft v4 - Go language
+// @created      28-02-2013
+
+package gojsonschema
+
+import (
+	"errors"
+	"fmt"
+)
+
+type JsonSchemaType struct {
+	types []string
+}
+
+func (t *JsonSchemaType) Add(etype string) error {
+
+	if !isStringInSlice(JSON_TYPES, etype) {
+		return errors.New(fmt.Sprintf("%s is not a valid type", etype))
+	}
+
+	if t.HasType(etype) {
+		return errors.New(fmt.Sprintf("%s type is duplicated", etype))
+	}
+
+	t.types = append(t.types, etype)
+
+	return nil
+}
+
+func (t *JsonSchemaType) HasType(etype string) bool {
+
+	for _, v := range t.types {
+		if v == etype {
+			return true
+		}
+	}
+
+	return false
+}
