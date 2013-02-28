583e97d34d320307702065722facd76eeeeeb1dd
sigu-399 <sigu.399@gmail.com>
2013-02-28 19:36:10 +0800

    moved schemaDocument consts to types.go

--- src/gojsonschema/types.go
+++ src/gojsonschema/types.go
@@ -3,6 +3,23 @@ package gojsonschema
 import ()
 
 const (
+	KEY_SCHEMA      = "$schema"
+	KEY_ID          = "$id"
+	KEY_REF         = "$ref"
+	KEY_TITLE       = "title"
+	KEY_DESCRIPTION = "description"
+	KEY_TYPE        = "type"
+	KEY_ITEMS       = "items"
+	KEY_PROPERTIES  = "properties"
+
+	STRING_STRING     = "string"
+	STRING_SCHEMA     = "schema"
+	STRING_PROPERTIES = "properties"
+
+	ROOT_SCHEMA_PROPERTY = "(root)"
+)
+
+const (
 	ERROR_MESSAGE_X_MUST_BE_OF_TYPE_Y = `%s must be of type %s`
 	ERROR_MESSAGE_X_IS_REQUIRED       = `%s is required`
 	ERROR_MESSAGE_X_MUST_BE_AN_OBJECT = `%s must be an object`
