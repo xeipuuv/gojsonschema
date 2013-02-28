a0b564f5620173cc88d6f8fcf4652a19861f2c4e
sigu-399 <sigu.399@gmail.com>
2013-02-28 22:53:07 +0800

    changed etype to types (JsonSchemaType)

--- src/gojsonschema/schema.go
+++ src/gojsonschema/schema.go
@@ -12,7 +12,7 @@ type JsonSchema struct {
 	id          *string
 	title       *string
 	description *string
-	etype       string
+	types       JsonSchemaType
 
 	ref *gojsonreference.JsonReference
 
