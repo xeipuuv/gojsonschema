68747874d15d53cb64675630a071654104089e23
sigu-399 <sigu.399@gmail.com>
2013-06-18 18:23:51 +0800

    fixed according to json test suite : mostly, not required fields and type checks

--- schemaDocument.go
+++ schemaDocument.go
@@ -52,6 +52,7 @@ func NewJsonSchemaDocument(document interface{}) (*JsonSchemaDocument, error) {
 		err = d.parse(spd.Document)
 
 	case map[string]interface{}:
+		d.documentReference, err = gojsonreference.NewJsonReference("#")
 		err = d.parse(document.(map[string]interface{}))
 
 	default:
@@ -87,20 +88,19 @@ func (d *JsonSchemaDocument) parseSchema(documentNode interface{}, currentSchema
 	m := documentNode.(map[string]interface{})
 
 	if currentSchema == d.rootSchema {
-		// We are in the initial schema
-		if !existsMapKey(m, KEY_SCHEMA) {
-			return errors.New(fmt.Sprintf(ERROR_MESSAGE_X_IS_REQUIRED, KEY_SCHEMA))
-		}
-		if !isKind(m[KEY_SCHEMA], reflect.String) {
-			return errors.New(fmt.Sprintf(ERROR_MESSAGE_X_MUST_BE_OF_TYPE_Y, KEY_SCHEMA, STRING_STRING))
-		}
-		schemaRef := m[KEY_SCHEMA].(string)
-		schemaReference, err := gojsonreference.NewJsonReference(schemaRef)
-		currentSchema.schema = &schemaReference
-		if err != nil {
-			return err
-		}
 
+		// We are in the initial schema
+		if existsMapKey(m, KEY_SCHEMA) {
+			if !isKind(m[KEY_SCHEMA], reflect.String) {
+				return errors.New(fmt.Sprintf(ERROR_MESSAGE_X_MUST_BE_OF_TYPE_Y, KEY_SCHEMA, STRING_STRING))
+			}
+			schemaRef := m[KEY_SCHEMA].(string)
+			schemaReference, err := gojsonreference.NewJsonReference(schemaRef)
+			currentSchema.schema = &schemaReference
+			if err != nil {
+				return err
+			}
+		}
 		currentSchema.ref = &d.documentReference
 	}
 
@@ -124,24 +124,24 @@ func (d *JsonSchemaDocument) parseSchema(documentNode interface{}, currentSchema
 			currentSchema.ref = inheritedReference
 		}
 
+		jsonPointer := currentSchema.ref.GetPointer()
+
 		dsp, err := d.pool.GetPoolDocument(*currentSchema.ref)
 		if err != nil {
 			return err
 		}
 
-		jsonPointer := currentSchema.ref.GetPointer()
-
-		httpDocumentNode, _, err := jsonPointer.Get(dsp.Document)
+		refdDocumentNode, _, err := jsonPointer.Get(dsp.Document)
 		if err != nil {
 			return err
 		}
 
-		if !isKind(httpDocumentNode, reflect.Map) {
+		if !isKind(refdDocumentNode, reflect.Map) {
 			return errors.New(fmt.Sprintf(ERROR_MESSAGE_X_MUST_BE_OF_TYPE_Y, STRING_SCHEMA, STRING_OBJECT))
 		}
 
 		// ref replaces current json structure with the one loaded just now
-		m = httpDocumentNode.(map[string]interface{})
+		m = refdDocumentNode.(map[string]interface{})
 	}
 
 	// id
@@ -169,40 +169,32 @@ func (d *JsonSchemaDocument) parseSchema(documentNode interface{}, currentSchema
 	}
 
 	// type
-	if !existsMapKey(m, KEY_TYPE) {
-		return errors.New(fmt.Sprintf(ERROR_MESSAGE_X_IS_REQUIRED, KEY_TYPE))
-	}
-
-	if isKind(m[KEY_TYPE], reflect.String) {
-
-		if k, ok := m[KEY_TYPE].(string); ok {
-			err := currentSchema.types.Add(k)
-			if err != nil {
-				return err
-			}
-		}
-	} else {
-		if isKind(m[KEY_TYPE], reflect.Slice) {
-			arrayOfTypes := m[KEY_TYPE].([]interface{})
-			for _, typeInArray := range arrayOfTypes {
-				if reflect.ValueOf(typeInArray).Kind() != reflect.String {
-					return errors.New(fmt.Sprintf(ERROR_MESSAGE_X_MUST_BE_OF_TYPE_Y, KEY_TYPE, STRING_STRING+"/"+STRING_ARRAY_OF_STRINGS))
-				} else {
-					currentSchema.types.Add(typeInArray.(string))
+	if existsMapKey(m, KEY_TYPE) {
+		if isKind(m[KEY_TYPE], reflect.String) {
+			if k, ok := m[KEY_TYPE].(string); ok {
+				err := currentSchema.types.Add(k)
+				if err != nil {
+					return err
 				}
 			}
-
 		} else {
-			return errors.New(fmt.Sprintf(ERROR_MESSAGE_X_MUST_BE_OF_TYPE_Y, KEY_TYPE, STRING_STRING+"/"+STRING_ARRAY_OF_STRINGS))
+			if isKind(m[KEY_TYPE], reflect.Slice) {
+				arrayOfTypes := m[KEY_TYPE].([]interface{})
+				for _, typeInArray := range arrayOfTypes {
+					if reflect.ValueOf(typeInArray).Kind() != reflect.String {
+						return errors.New(fmt.Sprintf(ERROR_MESSAGE_X_MUST_BE_OF_TYPE_Y, KEY_TYPE, STRING_STRING+"/"+STRING_ARRAY_OF_STRINGS))
+					} else {
+						currentSchema.types.Add(typeInArray.(string))
+					}
+				}
+
+			} else {
+				return errors.New(fmt.Sprintf(ERROR_MESSAGE_X_MUST_BE_OF_TYPE_Y, KEY_TYPE, STRING_STRING+"/"+STRING_ARRAY_OF_STRINGS))
+			}
 		}
 	}
-
 	// properties
-	if currentSchema.types.HasType(TYPE_OBJECT) {
-		if !existsMapKey(m, KEY_PROPERTIES) {
-			return errors.New(fmt.Sprintf(ERROR_MESSAGE_X_IS_REQUIRED, KEY_PROPERTIES))
-		}
-
+	if existsMapKey(m, KEY_PROPERTIES) {
 		for k := range m {
 			if k == KEY_PROPERTIES {
 				err := d.parseProperties(m[k], currentSchema)
@@ -214,10 +206,7 @@ func (d *JsonSchemaDocument) parseSchema(documentNode interface{}, currentSchema
 	}
 
 	// items
-	if currentSchema.types.HasType(TYPE_ARRAY) {
-		if !existsMapKey(m, KEY_ITEMS) {
-			return errors.New(fmt.Sprintf(ERROR_MESSAGE_X_IS_REQUIRED, KEY_ITEMS))
-		}
+	if existsMapKey(m, KEY_ITEMS) {
 
 		for k := range m {
 			if k == KEY_ITEMS {
@@ -238,76 +227,56 @@ func (d *JsonSchemaDocument) parseSchema(documentNode interface{}, currentSchema
 	// validation : number / integer
 
 	if existsMapKey(m, KEY_MULTIPLE_OF) {
-		if currentSchema.types.HasType(TYPE_NUMBER) || currentSchema.types.HasType(TYPE_INTEGER) {
-			if isKind(m[KEY_MULTIPLE_OF], reflect.Float64) {
-				multipleOfValue := m[KEY_MULTIPLE_OF].(float64)
-				if multipleOfValue <= 0 {
-					return errors.New("multipleOf must be strictly greater than 0")
-				}
-				currentSchema.multipleOf = &multipleOfValue
-			} else {
-				return errors.New("multipleOf must be a number")
+		if isKind(m[KEY_MULTIPLE_OF], reflect.Float64) {
+			multipleOfValue := m[KEY_MULTIPLE_OF].(float64)
+			if multipleOfValue <= 0 {
+				return errors.New("multipleOf must be strictly greater than 0")
 			}
+			currentSchema.multipleOf = &multipleOfValue
 		} else {
-			return errors.New("multipleOf applies to number,integer")
+			return errors.New("multipleOf must be a number")
 		}
 	}
 
 	if existsMapKey(m, KEY_MINIMUM) {
-		if currentSchema.types.HasType(TYPE_NUMBER) || currentSchema.types.HasType(TYPE_INTEGER) {
-			if isKind(m[KEY_MINIMUM], reflect.Float64) {
-				minimumValue := m[KEY_MINIMUM].(float64)
-				currentSchema.minimum = &minimumValue
-			} else {
-				return errors.New("minimum must be a number")
-			}
+		if isKind(m[KEY_MINIMUM], reflect.Float64) {
+			minimumValue := m[KEY_MINIMUM].(float64)
+			currentSchema.minimum = &minimumValue
 		} else {
-			return errors.New("minimum applies to number,integer")
+			return errors.New("minimum must be a number")
 		}
 	}
 
 	if existsMapKey(m, KEY_EXCLUSIVE_MINIMUM) {
-		if currentSchema.types.HasType(TYPE_NUMBER) || currentSchema.types.HasType(TYPE_INTEGER) {
-			if isKind(m[KEY_EXCLUSIVE_MINIMUM], reflect.Bool) {
-				if currentSchema.minimum == nil {
-					return errors.New("exclusiveMinimum cannot exist without minimum")
-				}
-				exclusiveMinimumValue := m[KEY_EXCLUSIVE_MINIMUM].(bool)
-				currentSchema.exclusiveMinimum = exclusiveMinimumValue
-			} else {
-				return errors.New("exclusiveMinimum must be a boolean")
+		if isKind(m[KEY_EXCLUSIVE_MINIMUM], reflect.Bool) {
+			if currentSchema.minimum == nil {
+				return errors.New("exclusiveMinimum cannot exist without minimum")
 			}
+			exclusiveMinimumValue := m[KEY_EXCLUSIVE_MINIMUM].(bool)
+			currentSchema.exclusiveMinimum = exclusiveMinimumValue
 		} else {
-			return errors.New("exclusiveMinimum applies to number,integer")
+			return errors.New("exclusiveMinimum must be a boolean")
 		}
 	}
 
 	if existsMapKey(m, KEY_MAXIMUM) {
-		if currentSchema.types.HasType(TYPE_NUMBER) || currentSchema.types.HasType(TYPE_INTEGER) {
-			if isKind(m[KEY_MAXIMUM], reflect.Float64) {
-				maximumValue := m[KEY_MAXIMUM].(float64)
-				currentSchema.maximum = &maximumValue
-			} else {
-				return errors.New("maximum must be a number")
-			}
+		if isKind(m[KEY_MAXIMUM], reflect.Float64) {
+			maximumValue := m[KEY_MAXIMUM].(float64)
+			currentSchema.maximum = &maximumValue
 		} else {
-			return errors.New("maximum applies to number,integer")
+			return errors.New("maximum must be a number")
 		}
 	}
 
 	if existsMapKey(m, KEY_EXCLUSIVE_MAXIMUM) {
-		if currentSchema.types.HasType(TYPE_NUMBER) || currentSchema.types.HasType(TYPE_INTEGER) {
-			if isKind(m[KEY_EXCLUSIVE_MAXIMUM], reflect.Bool) {
-				if currentSchema.maximum == nil {
-					return errors.New("exclusiveMaximum cannot exist without maximum")
-				}
-				exclusiveMaximumValue := m[KEY_EXCLUSIVE_MAXIMUM].(bool)
-				currentSchema.exclusiveMaximum = exclusiveMaximumValue
-			} else {
-				return errors.New("exclusiveMaximum must be a boolean")
+		if isKind(m[KEY_EXCLUSIVE_MAXIMUM], reflect.Bool) {
+			if currentSchema.maximum == nil {
+				return errors.New("exclusiveMaximum cannot exist without maximum")
 			}
+			exclusiveMaximumValue := m[KEY_EXCLUSIVE_MAXIMUM].(bool)
+			currentSchema.exclusiveMaximum = exclusiveMaximumValue
 		} else {
-			return errors.New("exclusiveMaximum applies to number,integer")
+			return errors.New("exclusiveMaximum must be a boolean")
 		}
 	}
 
@@ -320,44 +289,36 @@ func (d *JsonSchemaDocument) parseSchema(documentNode interface{}, currentSchema
 	// validation : string
 
 	if existsMapKey(m, KEY_MIN_LENGTH) {
-		if currentSchema.types.HasType(TYPE_STRING) {
-			if isKind(m[KEY_MIN_LENGTH], reflect.Float64) {
-				minLengthValue := m[KEY_MIN_LENGTH].(float64)
-				if isFloat64AnInteger(minLengthValue) {
-					if minLengthValue < 0 {
-						return errors.New("minLength must be greater than or equal to 0")
-					}
-					minLengthIntegerValue := int(minLengthValue)
-					currentSchema.minLength = &minLengthIntegerValue
-				} else {
-					return errors.New("minLength must be an integer")
+		if isKind(m[KEY_MIN_LENGTH], reflect.Float64) {
+			minLengthValue := m[KEY_MIN_LENGTH].(float64)
+			if isFloat64AnInteger(minLengthValue) {
+				if minLengthValue < 0 {
+					return errors.New("minLength must be greater than or equal to 0")
 				}
+				minLengthIntegerValue := int(minLengthValue)
+				currentSchema.minLength = &minLengthIntegerValue
 			} else {
 				return errors.New("minLength must be an integer")
 			}
 		} else {
-			return errors.New("minLength applies to string")
+			return errors.New("minLength must be an integer")
 		}
 	}
 
 	if existsMapKey(m, KEY_MAX_LENGTH) {
-		if currentSchema.types.HasType(TYPE_STRING) {
-			if isKind(m[KEY_MAX_LENGTH], reflect.Float64) {
-				maxLengthValue := m[KEY_MAX_LENGTH].(float64)
-				if isFloat64AnInteger(maxLengthValue) {
-					if maxLengthValue < 0 {
-						return errors.New("maxLength must be greater than or equal to 0")
-					}
-					maxLengthIntegerValue := int(maxLengthValue)
-					currentSchema.maxLength = &maxLengthIntegerValue
-				} else {
-					return errors.New("maxLength must be an integer")
+		if isKind(m[KEY_MAX_LENGTH], reflect.Float64) {
+			maxLengthValue := m[KEY_MAX_LENGTH].(float64)
+			if isFloat64AnInteger(maxLengthValue) {
+				if maxLengthValue < 0 {
+					return errors.New("maxLength must be greater than or equal to 0")
 				}
+				maxLengthIntegerValue := int(maxLengthValue)
+				currentSchema.maxLength = &maxLengthIntegerValue
 			} else {
 				return errors.New("maxLength must be an integer")
 			}
 		} else {
-			return errors.New("maxLength applies to string")
+			return errors.New("maxLength must be an integer")
 		}
 	}
 
@@ -368,62 +329,50 @@ func (d *JsonSchemaDocument) parseSchema(documentNode interface{}, currentSchema
 	}
 
 	if existsMapKey(m, KEY_PATTERN) {
-		if currentSchema.types.HasType(TYPE_STRING) {
-			if isKind(m[KEY_PATTERN], reflect.String) {
-				regexpObject, err := regexp.Compile(m[KEY_PATTERN].(string))
-				if err != nil {
-					return errors.New("pattern must be a valid regular expression")
-				}
-				currentSchema.pattern = regexpObject
-			} else {
-				return errors.New("pattern must be a string")
+		if isKind(m[KEY_PATTERN], reflect.String) {
+			regexpObject, err := regexp.Compile(m[KEY_PATTERN].(string))
+			if err != nil {
+				return errors.New("pattern must be a valid regular expression")
 			}
+			currentSchema.pattern = regexpObject
 		} else {
-			return errors.New("pattern applies to string")
+			return errors.New("pattern must be a string")
 		}
 	}
 
 	// validation : object
 
 	if existsMapKey(m, KEY_MIN_PROPERTIES) {
-		if currentSchema.types.HasType(TYPE_OBJECT) {
-			if isKind(m[KEY_MIN_PROPERTIES], reflect.Float64) {
-				minPropertiesValue := m[KEY_MIN_PROPERTIES].(float64)
-				if isFloat64AnInteger(minPropertiesValue) {
-					if minPropertiesValue < 0 {
-						return errors.New("minProperties must be greater than or equal to 0")
-					}
-					minPropertiesntegerValue := int(minPropertiesValue)
-					currentSchema.minProperties = &minPropertiesntegerValue
-				} else {
-					return errors.New("minProperties must be an integer")
+		if isKind(m[KEY_MIN_PROPERTIES], reflect.Float64) {
+			minPropertiesValue := m[KEY_MIN_PROPERTIES].(float64)
+			if isFloat64AnInteger(minPropertiesValue) {
+				if minPropertiesValue < 0 {
+					return errors.New("minProperties must be greater than or equal to 0")
 				}
+				minPropertiesntegerValue := int(minPropertiesValue)
+				currentSchema.minProperties = &minPropertiesntegerValue
 			} else {
 				return errors.New("minProperties must be an integer")
 			}
 		} else {
-			return errors.New("minProperties applies to object")
+			return errors.New("minProperties must be an integer")
 		}
 	}
 
 	if existsMapKey(m, KEY_MAX_PROPERTIES) {
-		if currentSchema.types.HasType(TYPE_OBJECT) {
-			if isKind(m[KEY_MAX_PROPERTIES], reflect.Float64) {
-				maxPropertiesValue := m[KEY_MAX_PROPERTIES].(float64)
-				if isFloat64AnInteger(maxPropertiesValue) {
-					if maxPropertiesValue < 0 {
-						return errors.New("maxProperties must be greater than or equal to 0")
-					}
-					maxPropertiesntegerValue := int(maxPropertiesValue)
-					currentSchema.maxProperties = &maxPropertiesntegerValue
-				} else {
-					return errors.New("maxProperties must be an integer")
+		if isKind(m[KEY_MAX_PROPERTIES], reflect.Float64) {
+			maxPropertiesValue := m[KEY_MAX_PROPERTIES].(float64)
+			if isFloat64AnInteger(maxPropertiesValue) {
+				if maxPropertiesValue < 0 {
+					return errors.New("maxProperties must be greater than or equal to 0")
 				}
+				maxPropertiesntegerValue := int(maxPropertiesValue)
+				currentSchema.maxProperties = &maxPropertiesntegerValue
 			} else {
 				return errors.New("maxProperties must be an integer")
 			}
 		} else {
-			return errors.New("maxProperties applies to object")
+			return errors.New("maxProperties must be an integer")
 		}
 	}
 
@@ -434,80 +383,64 @@ func (d *JsonSchemaDocument) parseSchema(documentNode interface{}, currentSchema
 	}
 
 	if existsMapKey(m, KEY_REQUIRED) {
-		if currentSchema.types.HasType(TYPE_OBJECT) {
-			if isKind(m[KEY_REQUIRED], reflect.Slice) {
-				requiredValues := m[KEY_REQUIRED].([]interface{})
-				for _, requiredValue := range requiredValues {
-					if isKind(requiredValue, reflect.String) {
-						err := currentSchema.AddRequired(requiredValue.(string))
-						if err != nil {
-							return err
-						}
-					} else {
-						return errors.New("required items must be string")
+		if isKind(m[KEY_REQUIRED], reflect.Slice) {
+			requiredValues := m[KEY_REQUIRED].([]interface{})
+			for _, requiredValue := range requiredValues {
+				if isKind(requiredValue, reflect.String) {
+					err := currentSchema.AddRequired(requiredValue.(string))
+					if err != nil {
+						return err
 					}
+				} else {
+					return errors.New("required items must be string")
 				}
-			} else {
-				return errors.New("required must be an array")
 			}
 		} else {
-			return errors.New("required applies to object")
+			return errors.New("required must be an array")
 		}
 	}
 
 	// validation : array
 
 	if existsMapKey(m, KEY_MIN_ITEMS) {
-		if currentSchema.types.HasType(TYPE_ARRAY) {
-			if isKind(m[KEY_MIN_ITEMS], reflect.Float64) {
-				minItemsValue := m[KEY_MIN_ITEMS].(float64)
-				if isFloat64AnInteger(minItemsValue) {
-					if minItemsValue < 0 {
-						return errors.New("minItems must be greater than or equal to 0")
-					}
-					minItemsIntegerValue := int(minItemsValue)
-					currentSchema.minItems = &minItemsIntegerValue
-				} else {
-					return errors.New("minItems must be an integer")
+		if isKind(m[KEY_MIN_ITEMS], reflect.Float64) {
+			minItemsValue := m[KEY_MIN_ITEMS].(float64)
+			if isFloat64AnInteger(minItemsValue) {
+				if minItemsValue < 0 {
+					return errors.New("minItems must be greater than or equal to 0")
 				}
+				minItemsIntegerValue := int(minItemsValue)
+				currentSchema.minItems = &minItemsIntegerValue
 			} else {
 				return errors.New("minItems must be an integer")
 			}
 		} else {
-			return errors.New("minItems applies to array")
+			return errors.New("minItems must be an integer")
 		}
 	}
 
 	if existsMapKey(m, KEY_MAX_ITEMS) {
-		if currentSchema.types.HasType(TYPE_ARRAY) {
-			if isKind(m[KEY_MAX_ITEMS], reflect.Float64) {
-				maxItemsValue := m[KEY_MAX_ITEMS].(float64)
-				if isFloat64AnInteger(maxItemsValue) {
-					if maxItemsValue < 0 {
-						return errors.New("maxItems must be greater than or equal to 0")
-					}
-					maxItemsIntegerValue := int(maxItemsValue)
-					currentSchema.maxItems = &maxItemsIntegerValue
-				} else {
-					return errors.New("maxItems must be an integer")
+		if isKind(m[KEY_MAX_ITEMS], reflect.Float64) {
+			maxItemsValue := m[KEY_MAX_ITEMS].(float64)
+			if isFloat64AnInteger(maxItemsValue) {
+				if maxItemsValue < 0 {
+					return errors.New("maxItems must be greater than or equal to 0")
 				}
+				maxItemsIntegerValue := int(maxItemsValue)
+				currentSchema.maxItems = &maxItemsIntegerValue
 			} else {
 				return errors.New("maxItems must be an integer")
 			}
 		} else {
-			return errors.New("maxItems applies to array")
+			return errors.New("maxItems must be an integer")
 		}
 	}
 
 	if existsMapKey(m, KEY_UNIQUE_ITEMS) {
-		if currentSchema.types.HasType(TYPE_ARRAY) {
-			if isKind(m[KEY_UNIQUE_ITEMS], reflect.Bool) {
-				currentSchema.uniqueItems = m[KEY_UNIQUE_ITEMS].(bool)
-			} else {
-				return errors.New("uniqueItems must be an boolean")
-			}
+		if isKind(m[KEY_UNIQUE_ITEMS], reflect.Bool) {
+			currentSchema.uniqueItems = m[KEY_UNIQUE_ITEMS].(bool)
 		} else {
-			return errors.New("uniqueItems applies to array")
+			return errors.New("uniqueItems must be an boolean")
 		}
 	}
 
